9b3a4bb70f6c7852b186b2e434a776fc642f01e3
Mike Lyons <mike.lyons@courtagen.com>
2012-09-12 11:31:02 -0400

    added Resource object to hold constants and default values.
    Also addded SGETest

--- src/test/scala/org/github/mylons/test/SGETest.scala
+++ src/test/scala/org/github/mylons/test/SGETest.scala
@@ -1,11 +1,23 @@
 package org.github.mylons.test
 
+import org.github.mylons.sge.Job
+
 /**
  * Author: Mike Lyons
  * Date: 9/10/12
  * Time: 1:56 PM
  * Description:
  */
-class SGETest {
+class SGETest( nameOfJob: String ) extends Job {
+  override val jobName = nameOfJob
+  override val NUMBER_OF_CPUS = 10
+  this.appendNameToScript()
+  appendCommandToScript("""echo "hello world" """)
+}
 
+object SGETest extends App {
+  val sge = new SGETest("test-name")
+  println( sge.script().reduceLeft(_ + "\n" + _))
+  println("\n\n")
+  println( sge.script().reduceLeft(_ + "\n" + _))
 }
